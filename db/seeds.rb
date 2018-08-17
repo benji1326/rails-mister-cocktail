6f006173482f49b2d0883a7087cb7cd88d121dd7
Benjamin Myara <benjaminmyara@me.com>
2018-08-17 14:09:24 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Cleaning database...'
+Cocktail.destroy_all
+Ingredient.destroy_all
+Dose.destroy_all
+
+
+url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)
+
+ingredients["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient['strIngredient1'])
+end
+
+
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "mango")
+Cocktail.create(name: "Bloody Marry")
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Manhattan")
+Cocktail.create(name: "Espresso Martini")
+Cocktail.create(name: "Black Russian")
+Cocktail.create(name: "Frisky Bison")
+Cocktail.create(name: "Virgin Marry")
+Cocktail.create(name: "Long Island")
+Cocktail.create(name: "Summer Punch")
+Cocktail.create(name: "Dry Martini")
+Cocktail.create(name: "Black Russian")
+Cocktail.create(name: "Caipirinha")
+Cocktail.create(name: "Strawberry Daiquiri")
+
+puts 'finished ingredients'
